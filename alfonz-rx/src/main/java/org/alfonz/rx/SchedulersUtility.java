5a96d1414f5187bfc8bbce3d25a611502390f25b
Petr Nohejl <petr.nohejl@gmail.com>
2017-04-09 11:40:25 +0200

    Lazy initialization of transformers

--- alfonz-rx/src/main/java/org/alfonz/rx/SchedulersUtility.java
+++ alfonz-rx/src/main/java/org/alfonz/rx/SchedulersUtility.java
@@ -1,17 +1,21 @@
 package org.alfonz.rx;
 
+import io.reactivex.CompletableTransformer;
 import io.reactivex.FlowableTransformer;
+import io.reactivex.MaybeTransformer;
 import io.reactivex.ObservableTransformer;
+import io.reactivex.SingleTransformer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 
 public final class SchedulersUtility
 {
-	private static final ObservableTransformer<?, ?> sSchedulersObservableTransformer =
-			observable -> observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
-	private static final FlowableTransformer<?, ?> sSchedulersFlowableTransformer =
-			flowable -> flowable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+	private static ObservableTransformer<?, ?> sSchedulersObservableTransformer;
+	private static SingleTransformer<?, ?> sSchedulersSingleTransformer;
+	private static CompletableTransformer sSchedulersCompletableTransformer;
+	private static MaybeTransformer<?, ?> sSchedulersMaybeTransformer;
+	private static FlowableTransformer<?, ?> sSchedulersFlowableTransformer;
 
 
 	private SchedulersUtility() {}
@@ -20,13 +24,53 @@ public final class SchedulersUtility
 	@SuppressWarnings("unchecked")
 	public static <T> ObservableTransformer<T, T> applyObservableSchedulers()
 	{
+		if(sSchedulersObservableTransformer == null)
+		{
+			sSchedulersObservableTransformer = observable -> observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+		}
 		return (ObservableTransformer<T, T>) sSchedulersObservableTransformer;
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public static <T> SingleTransformer<T, T> applySingleSchedulers()
+	{
+		if(sSchedulersSingleTransformer == null)
+		{
+			sSchedulersSingleTransformer = single -> single.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+		}
+		return (SingleTransformer<T, T>) sSchedulersSingleTransformer;
+	}
+
+
+	public static <T> CompletableTransformer applyCompletableSchedulers()
+	{
+		if(sSchedulersCompletableTransformer == null)
+		{
+			sSchedulersCompletableTransformer = completable -> completable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+		}
+		return sSchedulersCompletableTransformer;
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public static <T> MaybeTransformer<T, T> applyMaybeSchedulers()
+	{
+		if(sSchedulersMaybeTransformer == null)
+		{
+			sSchedulersMaybeTransformer = maybe -> maybe.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+		}
+		return (MaybeTransformer<T, T>) sSchedulersMaybeTransformer;
 	}
 
 
 	@SuppressWarnings("unchecked")
 	public static <T> FlowableTransformer<T, T> applyFlowableSchedulers()
 	{
+		if(sSchedulersFlowableTransformer == null)
+		{
+			sSchedulersFlowableTransformer = flowable -> flowable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+		}
 		return (FlowableTransformer<T, T>) sSchedulersFlowableTransformer;
 	}
 }
