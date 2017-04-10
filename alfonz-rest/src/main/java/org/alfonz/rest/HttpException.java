56887e29d25fa20e32607149b8d2cae7f418dfbf
Petr Nohejl <petr.nohejl@gmail.com>
2017-04-10 14:10:48 +0200

    HttpException in rest module now extends newly added retrofit2.HttpException

--- alfonz-rest/src/main/java/org/alfonz/rest/HttpException.java
+++ alfonz-rest/src/main/java/org/alfonz/rest/HttpException.java
@@ -3,44 +3,19 @@ package org.alfonz.rest;
 import retrofit2.Response;
 
 
-// inspired by: retrofit2.adapter.rxjava.HttpException
-public abstract class HttpException extends Exception
+public abstract class HttpException extends retrofit2.HttpException
 {
-	private final int mCode;
-	private final String mMessage;
-	private final transient Response<?> mResponse;
 	private final Object mError;
 
 
 	public HttpException(Response<?> response)
 	{
-		super("HTTP " + response.code() + " " + response.message());
-		mCode = response.code();
-		mMessage = response.message();
-		mResponse = response;
+		super(response);
 		mError = parseError(response);
 	}
 
 
 	public abstract Object parseError(Response<?> response);
-
-
-	public int code()
-	{
-		return mCode;
-	}
-
-
-	public String message()
-	{
-		return mMessage;
-	}
-
-
-	public Response<?> response()
-	{
-		return mResponse;
-	}
 
 
 	public Object error()
