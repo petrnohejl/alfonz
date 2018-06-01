3c5d1cedba06b515faaf5f883f0aaae618e553fa
Petr Nohejl <petr.nohejl@gmail.com>
2018-06-01 14:45:25 +0200

    Add @RequiresApi annotation

--- alfonz-utility/src/main/java/org/alfonz/utility/IntentUtility.java
+++ alfonz-utility/src/main/java/org/alfonz/utility/IntentUtility.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 
 import java.util.List;
 import java.util.Locale;
@@ -222,6 +223,7 @@ public final class IntentUtility
 	}
 
 
+	@RequiresApi(Build.VERSION_CODES.KITKAT)
 	public static void startNotificationSettingsActivity(@NonNull Context context)
 	{
 		try
